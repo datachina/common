8d7c5f0fde0170c166b4ecea7fd78fb1ab5201a2
jidaojiuyou <2320541805@qq.com>
2022-04-26 19:53:42 +0800

    通用异常类

---
+++ common-lang/src/main/java/lang/exception/BaseException.java
@@ -0,0 +1,37 @@
+package lang.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 公共异常类
+ *
+ * @author jidaojiuyou
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@SuppressWarnings("unused")
+public class BaseException extends RuntimeException {
+
+    private String code;
+
+    /**
+     * @param message 异常消息
+     */
+    public BaseException(String message) {
+        super(message);
+    }
+
+    /**
+     * @param code    错误码
+     * @param message 错误消息
+     */
+    public BaseException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
