8afeedce37bb1f4eca516fed1e9e32de1faa2c37
jidaojiuyou <2320541805@qq.com>
2022-04-27 16:46:29 +0800

    自动配置

---
+++ common-web-starter/src/main/java/com/data/common/web/starter/config/CommonWebAutoConfiguration.java
@@ -0,0 +1,27 @@
+package com.data.common.web.starter.config;
+
+import com.data.common.web.jackson.JacksonModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自动包扫描
+ *
+ * @author jidaojiuyou
+ */
+@ComponentScan("com.data.common")
+@Configuration
+public class CommonWebAutoConfiguration {
+
+    /**
+     * 注入JavaTimeModule
+     *
+     * @return JavaTimeModule
+     */
+    @Bean
+    public JavaTimeModule javaTimeModule() {
+        return JacksonModule.getJavaTimeModule();
+    }
+}
