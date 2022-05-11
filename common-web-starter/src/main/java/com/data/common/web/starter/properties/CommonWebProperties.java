427f18bf5820ea9ce9c06e258f6ad11c5c4762dc
jidaojiuyou <2320541805@qq.com>
2022-05-11 15:55:34 +0800

    修改注释

--- common-web-starter/src/main/java/com/data/common/web/starter/properties/CommonWebProperties.java
+++ common-web-starter/src/main/java/com/data/common/web/starter/properties/CommonWebProperties.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * 配置
+ * 配置类
  *
  * @author jidaojiuyou
  */
@@ -16,167 +16,167 @@ public class CommonWebProperties {
     /**
      * 异常拦截器配置
      */
-    private ExceptionHandler exceptionHandler;
+    private ExceptionHandlerConfig exceptionHandler;
     /**
      * 跨域配置
      */
     private DefaultCorsConfig defaultCors;
 
     /**
-     * 跨域配置
+     * 跨域配置类
      */
     @Getter
     @Setter
-    public static class DefaultCorsConfig{
+    public static class DefaultCorsConfig {
         /**
-         * 是否启用
+         * 是否启用默认跨域配置.
          */
         private Boolean enable = false;
     }
 
     /**
-     * 异常拦截器
+     * 异常拦截器配置类
      */
     @Getter
     @Setter
-    public static class ExceptionHandler {
+    public static class ExceptionHandlerConfig {
         /**
-         * 全局配置
+         * 异常拦截器全局配置
          */
         private Global global;
         /**
-         * 新增异常拦截器的配置
+         * 新增异常拦截器配置
          */
         private Add add;
         /**
-         * 删除异常拦截器的配置
+         * 删除异常拦截器配置
          */
         private Delete delete;
         /**
-         * 修改异常拦截器的配置
+         * 修改异常拦截器配置
          */
         private Update update;
         /**
-         * 查询异常拦截器的配置
+         * 查询异常拦截器配置
          */
         private Query query;
         /**
-         * 参数校验异常拦截器的配置
+         * 参数校验异常拦截器配置
          */
         private Validate validate;
 
         /**
-         * 全局配置
+         * 异常拦截器全局配置类
          */
         @Getter
         @Setter
         public static class Global {
             /**
-             * 是否打印错误栈
+             * 是否全局启用打印异常栈.
              */
             private Boolean printStackTrace = false;
             /**
-             * 是否打印日志
+             * 是否全局启用打印错误日志.
              */
             private Boolean printLog = true;
         }
 
         /**
-         * 新增异常拦截器的配置
+         * 新增异常拦截器配置类
          */
         @Getter
         @Setter
         public static class Add {
             /**
-             * 是否启用
+             * 是否启用新增异常拦截器.
              */
             private Boolean enable = false;
             /**
-             * 是否打印错误栈
+             * 是否启用新增异常拦截器打印异常栈.
              */
             private Boolean printStackTrace;
             /**
-             * 是否打印日志
+             * 是否启用新增异常拦截器打印错误日志.
              */
             private Boolean printLog;
         }
 
         /**
-         * 删除异常拦截器的配置
+         * 删除异常拦截器配置类
          */
         @Getter
         @Setter
         public static class Delete {
             /**
-             * 是否启用
+             * 是否启用删除异常拦截器.
              */
             private Boolean enable = false;
             /**
-             * 是否打印错误栈
+             * 是否启用删除异常拦截器打印异常栈.
              */
             private Boolean printStackTrace;
             /**
-             * 是否打印日志
+             * 是否启用删除异常拦截器打印错误日志.
              */
             private Boolean printLog;
         }
 
         /**
-         * 修改异常拦截器的配置
+         * 修改异常拦截器配置类
          */
         @Getter
         @Setter
         public static class Update {
             /**
-             * 是否启用
+             * 是否启用修改异常拦截器.
              */
             private Boolean enable = false;
             /**
-             * 是否打印错误栈
+             * 是否启用修改异常拦截器打印异常栈.
              */
             private Boolean printStackTrace;
             /**
-             * 是否打印日志
+             * 是否启用修改异常拦截器打印错误日志.
              */
             private Boolean printLog;
         }
 
         /**
-         * 查询异常拦截器的配置
+         * 查询异常拦截器配置类
          */
         @Getter
         @Setter
         public static class Query {
             /**
-             * 是否启用
+             * 是否启用查询异常拦截器.
              */
             private Boolean enable = false;
             /**
-             * 是否打印错误栈
+             * 是否启用查询异常拦截器打印异常栈.
              */
             private Boolean printStackTrace;
             /**
-             * 是否打印日志
+             * 是否启用查询异常拦截器打印错误日志.
              */
             private Boolean printLog;
         }
 
         /**
-         * 参数校验异常拦截器的配置
+         * 参数校验异常拦截器配置类
          */
         @Getter
         @Setter
         public static class Validate {
             /**
-             * 是否启用
+             * 是否启用参数校验异常拦截器.
              */
             private Boolean enable = false;
             /**
-             * 是否打印错误栈
+             * 是否启用参数校验异常拦截器打印异常栈.
              */
             private Boolean printStackTrace;
             /**
-             * 是否打印日志
+             * 是否启用参数校验异常拦截器打印错误日志.
              */
             private Boolean printLog;
         }
