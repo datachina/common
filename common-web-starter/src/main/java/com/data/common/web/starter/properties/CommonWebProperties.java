d0517dae13597eeeb7bfef184b2a6e1f17ac03fe
jidaojiuyou <2320541805@qq.com>
2022-05-11 15:25:01 +0800

    常用配置

--- common-web-starter/src/main/java/com/data/common/web/starter/properties/CommonWebProperties.java
+++ common-web-starter/src/main/java/com/data/common/web/starter/properties/CommonWebProperties.java
@@ -14,29 +14,171 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Setter
 public class CommonWebProperties {
     /**
-     * 日志配置
+     * 异常拦截器配置
      */
-    private Log log = new Log();
+    private ExceptionHandler exceptionHandler;
     /**
-     * 异常拦截配置
+     * 跨域配置
      */
-    private ExceptionHandler exceptionHandler = new ExceptionHandler();
+    private DefaultCorsConfig defaultCors;
 
     /**
-     * 日志配置
+     * 跨域配置
      */
     @Getter
     @Setter
-    public static class Log {
-        private Boolean enabled = true;
+    public static class DefaultCorsConfig{
+        /**
+         * 是否启用
+         */
+        private Boolean enable = false;
     }
 
     /**
-     * 异常拦截
+     * 异常拦截器
      */
     @Getter
     @Setter
     public static class ExceptionHandler {
-        private Boolean printStackTrace = true;
+        /**
+         * 全局配置
+         */
+        private Global global;
+        /**
+         * 新增异常拦截器的配置
+         */
+        private Add add;
+        /**
+         * 删除异常拦截器的配置
+         */
+        private Delete delete;
+        /**
+         * 修改异常拦截器的配置
+         */
+        private Update update;
+        /**
+         * 查询异常拦截器的配置
+         */
+        private Query query;
+        /**
+         * 参数校验异常拦截器的配置
+         */
+        private Validate validate;
+
+        /**
+         * 全局配置
+         */
+        @Getter
+        @Setter
+        public static class Global {
+            /**
+             * 是否打印错误栈
+             */
+            private Boolean printStackTrace = false;
+            /**
+             * 是否打印日志
+             */
+            private Boolean printLog = true;
+        }
+
+        /**
+         * 新增异常拦截器的配置
+         */
+        @Getter
+        @Setter
+        public static class Add {
+            /**
+             * 是否启用
+             */
+            private Boolean enable = false;
+            /**
+             * 是否打印错误栈
+             */
+            private Boolean printStackTrace;
+            /**
+             * 是否打印日志
+             */
+            private Boolean printLog;
+        }
+
+        /**
+         * 删除异常拦截器的配置
+         */
+        @Getter
+        @Setter
+        public static class Delete {
+            /**
+             * 是否启用
+             */
+            private Boolean enable = false;
+            /**
+             * 是否打印错误栈
+             */
+            private Boolean printStackTrace;
+            /**
+             * 是否打印日志
+             */
+            private Boolean printLog;
+        }
+
+        /**
+         * 修改异常拦截器的配置
+         */
+        @Getter
+        @Setter
+        public static class Update {
+            /**
+             * 是否启用
+             */
+            private Boolean enable = false;
+            /**
+             * 是否打印错误栈
+             */
+            private Boolean printStackTrace;
+            /**
+             * 是否打印日志
+             */
+            private Boolean printLog;
+        }
+
+        /**
+         * 查询异常拦截器的配置
+         */
+        @Getter
+        @Setter
+        public static class Query {
+            /**
+             * 是否启用
+             */
+            private Boolean enable = false;
+            /**
+             * 是否打印错误栈
+             */
+            private Boolean printStackTrace;
+            /**
+             * 是否打印日志
+             */
+            private Boolean printLog;
+        }
+
+        /**
+         * 参数校验异常拦截器的配置
+         */
+        @Getter
+        @Setter
+        public static class Validate {
+            /**
+             * 是否启用
+             */
+            private Boolean enable = false;
+            /**
+             * 是否打印错误栈
+             */
+            private Boolean printStackTrace;
+            /**
+             * 是否打印日志
+             */
+            private Boolean printLog;
+        }
     }
 }
